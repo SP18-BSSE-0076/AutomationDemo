5c74d27bb9019b74deb3c7c3365d8d2599c1c57d
SP18-BSSE-0076 <61426603+SP18-BSSE-0076@users.noreply.github.com>
2022-01-17 15:09:24 +0500

    Update AutomationDemo.bat

--- AutomationDemo.bat
+++ AutomationDemo.bat
@@ -1,16 +1,18 @@
 @ECHO OFF
 ECHO Demo Automation Execution Started.
 
-set testcategory=SignIn
+set testCategory1=SignIn
+set testCategory2=SignInTwo
 set dllpath=D:\OneDrive - Constellation HomeBuilder Systems\Automation\AutomationDemo\bin\Debug\AutomationDemo.dll
 set trxerpath=D:\OneDrive - Constellation HomeBuilder Systems\Automation\AutomationDemo\bin\Debug
 set reportpath=D:\OneDrive - Constellation HomeBuilder Systems\Automation\AutomationDemo\Reporting
 set vsCmdRunner=C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat
+set trxerConsolePath=D:\OneDrive - Constellation HomeBuilder Systems\Automation\AutomationDemo\bin\Debug\TrxerConsole.exe
 
 call "%vsCmdRunner%"
-VSTest.Console.exe "%dllpath%" /TestCaseFilter:"TestCategory=%testcategory%" /Logger:"trx;LogFileName=%reportpath%\Test1.trx"
+VSTest.Console.exe "%dllpath%" /TestCaseFilter:"(TestCategory=%testcategory1% | TestCategory=%testcategory2%)" /Logger:"trx;LogFileName=%reportpath%\Test2"
 
 cd "%trxerpath%"
-TrxerConsole.exe "%reportpath%"\Test1.trx
+"%trxerConsolePath%" "%reportpath%"\Test2
 
 PAUSE
